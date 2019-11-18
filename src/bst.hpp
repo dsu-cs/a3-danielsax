afe7dddbfb655f81d08490b64e17eca3a3a19ba2
danielsax <daniel.sax@trojans.dsu.edu>
2019-11-18 02:55:35 -0600

    Update bst.hpp
    Implemented the rest of the functions.
    and commented remove

--- src/bst.hpp
+++ src/bst.hpp
@@ -37,7 +37,6 @@ public:
     // returns: the number of nodes in the tree
     int get_size(void);
 
-    void printVec(std::vector<T> *);
 private:
     // the root node of the tree
     Node<T> *root;
@@ -45,6 +44,9 @@ private:
     int node_count;
     Node<T> *insertHelper(T, Node<T> *);
     std::vector<T> *inorderHelper(Node<T> *, std::vector<T> *);
+    std::vector<T> *preorderHelper(Node<T> *, std::vector<T> *);
+    std::vector<T> *postorderHelper(Node<T> *, std::vector<T> *);
+    Node<T> *searchHelper(T, Node<T> *);
 };
 
 template<class T>
@@ -68,16 +70,16 @@ template<class T>
  std::vector<T> * BST<T>::inorder()
 {
     std::vector<T> *vec = new std::vector<T>;
-    vec = inorderHelper(root, vec);
+    BST<T>::inorderHelper(root, vec);
     return vec;
 }
 
 template<class T>
- std::vector<T> * inorderHelper(Node<T> *sub_root, std::vector<T> *head)
+ std::vector<T> * BST<T>::inorderHelper(Node<T> *sub_root, std::vector<T> *head)
 {
     if (sub_root != NULL) {
         inorderHelper(sub_root->get_left(), head);
-        head.push_back(sub_root->get_data());
+        head->push_back(sub_root->get_data());
         inorderHelper(sub_root->get_right(), head);
     }
     return head;
@@ -87,16 +89,38 @@ template<class T>
  std::vector<T> * BST<T>::preorder()
 {
     std::vector<T> *vec = new std::vector<T>;
+    BST<T>::preorderHelper(root, vec);
     return vec;
 }
 
+template<class T>
+ std::vector<T> * BST<T>::preorderHelper(Node<T> *sub_root, std::vector<T> *head)
+{
+    if (sub_root != NULL) {
+        head->push_back(sub_root->get_data());
+        preorderHelper(sub_root->get_left(), head);
+        preorderHelper(sub_root->get_right(), head);
+    }
+    return head;
+}
 
 template<class T>
  std::vector<T> * BST<T>::postorder()
 {
     std::vector<T> *vec = new std::vector<T>;
-
+    BST<T>::postorderHelper(root, vec);
     return vec;
+}
+
+template<class T>
+ std::vector<T> * BST<T>::postorderHelper(Node<T> *sub_root, std::vector<T> *head)
+{
+    if (sub_root != NULL) {
+        postorderHelper(sub_root->get_left(), head);
+        postorderHelper(sub_root->get_right(), head);
+        head->push_back(sub_root->get_data());
+    }
+    return head;
 }
 
 template<class T>
@@ -115,46 +139,118 @@ Node<T> * BST<T>::insertHelper(T new_data, Node<T> *sub_root)
         return sub_root;
     }
     else if (sub_root->get_data() == new_data) {
-        return NULL;
+        return sub_root;
     }
     else if (new_data < sub_root->get_data()) {
-        insertHelper(new_data, sub_root->get_left());
+        sub_root->set_left(insertHelper(new_data, sub_root->get_left()));
     }
     else {
-        insertHelper(new_data, sub_root->get_right());
+        sub_root->set_right(insertHelper(new_data, sub_root->get_right()));
     }
-    return 0;
+    return sub_root;
 }
 
 
 template<class T>
 Node<T> *BST<T>::search(T val)
 {
-
+    return BST<T>::searchHelper(val, root);
 }
 
-
+template<class T>
+Node<T> *BST<T>::searchHelper(T val, Node<T> *sub_root)
+{
+    if (sub_root == NULL) {
+        return NULL;
+    }
+    else if (sub_root->get_data() == val) {
+        return sub_root;
+    }
+    else if (val < sub_root->get_data()) {
+        return BST<T>::searchHelper(val, sub_root->get_left());
+    }
+    else if (val > sub_root->get_data()) {
+        return BST<T>::searchHelper(val, sub_root->get_right());
+    }
+}
 
 template<class T>
 void BST<T>::remove(T val)
 {
-
+    Node<T> * del;                                                          //finds node to delete
+    Node<T> * trail;                                                        //trails del inorder skip past
+    Node<T> * replace;
+    T replaceVal;
+    del = root;
+    trail = NULL;
+    while (del != NULL) {                                                   //loop finds node to be deleted
+        if (del->get_data() == val) {                                       //Node found
+            if (del->get_left() == NULL && del->get_right() == NULL) {      //For leaf case
+                if (trail == NULL) {                                        //Handles root case
+                    root = NULL;
+                    delete(del);
+                } 
+                else if (del == trail->get_right()) {                       //Handles right case
+                    trail->set_right(NULL);
+                    delete(del);
+                }
+                else {                                                      //Handles left case
+                    trail->set_left(NULL);
+                    delete(del);            
+                }
+            }
+            else if (del->get_left() != NULL && del->get_right() != NULL) { //Handles two child case
+                replace = del->get_left();
+                while (replace->get_right() != NULL) {                      //Searches for largest Node in right branch
+                    replace = replace->get_right();
+                }
+                replaceVal = replace->get_data();
+                BST<T>::remove(replaceVal);
+                del->set_data(replaceVal);
+            }
+            else if (del->get_left() != NULL) {                               //Handles one left child case
+                if (trail == NULL) {                                        //Handles root case
+                    root = del->get_left();
+                    delete(del);
+                }
+                else if (trail->get_right() == del) {                       //Handles right case
+                    trail->set_right(del->get_left());
+                    delete(del);
+                }
+                else {                                                      //Handles left case
+                    trail->set_left(del->get_left());
+                    delete(del);
+                }
+            }
+            else {                                                          //Handles one right child case
+                if (trail == NULL) {                                        //Handles root case
+                    root = del->get_right();
+                    delete(del);
+                }
+                else if (trail->get_right() == del) {                       //Handles right case
+                    trail->set_right(del->get_right());
+                    delete(del);
+                }
+                else {                                                      //Handles left case
+                    trail->set_left(del->get_right());
+                    delete(del);
+                }
+            }
+            return;
+        }
+        else if (val < del->get_data()) {                                   //searches left case
+            trail = del;
+            del = del->get_left();
+        }
+        else {                                                              //searches right case
+            trail = del;
+            trail = del->get_left();
+        }
+    }
 }
-
-
 
 template<class T>
 int BST<T>::get_size()
 {
-
-}
-
-template<class T>
-void BST<T>::printVec(std::vector<T> *head)
-{
-    while (!head->empty()) {
-        std::cout << head->back() << " ";
-        head->pop_back();
-    }
-    std::cout << std::endl;
+    return node_count;
 }
